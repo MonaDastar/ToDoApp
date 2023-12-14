c4dc89b2f12bd0c6bc17a08fb209d52cc6085a96
Mona Dastar <-c>
2023-12-14 01:18:03 -0500

    customize the Django admin interface for your Task and User models

--- task/admin.py
+++ task/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from task.models import Task, User
 
-admin.site.register(User)
-admin.site.register(Task)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'address', 'createdAt', 'updatedAt')
+    search_fields = ('username', 'address')
+    list_filter = ('createdAt', 'updatedAt')
 
-#cusomize the table by modelAdmin
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'due_date', 'done', 'user', 'createdAt', 'updatedAt')
+    search_fields = ('title', 'description', 'user__username')
+    list_filter = ('due_date', 'done', 'createdAt', 'updatedAt')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Task, TaskAdmin)
+
