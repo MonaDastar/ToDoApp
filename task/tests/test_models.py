bf5c3c0b6d216db7586fa6a51714e3d18ddcabf0
Mona Dastar <-c>
2023-12-16 23:41:20 -0500

    add tests for Task Model

--- task/tests/test_models.py
+++ task/tests/test_models.py
@@ -34,5 +34,36 @@ class UserModelTest(TestCase):
         user.delete()
         self.assertEqual(User.objects.count(),user_count)
         
+class TaskModelTest(TestCase):
     
+    def setUp(self):
+        self.user = User.objects.create(username="testuser2", address="Test Address2")
+
+    
+    def test_create_task(self):
+        
+        task = Task.objects.create(title="clean the house", user=self.user)
+        task_user_title = Task.objects.get(title="clean the house",user=self.user)
+        self.assertEqual(task_user_title,task)
+        
+    def test_update_task(self):
+        task = Task.objects.create(title="test task", user=self.user)
+        
+        task.title = "updated_title"
+        task.save()
+        
+        updated_task = Task.objects.get(id=task.id)
+    
+        self.assertEqual(updated_task.title,"updated_title")
+        
+    def test_delete_task(self):
+        
+        task = Task.objects.create(title="test task", user=self.user)
+        
+        task_count=Task.objects.count()
+        
+        task.delete()
+        
+        self.assertEqual(Task.objects.count(), task_count-1)
+        
     
